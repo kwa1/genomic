d9791ac5d50561db33fc898021d1c020b7e9f7c2
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-27 22:33:49 +0000

    Update variables.tf

--- module/IAM/role/variables.tf
+++ module/IAM/role/variables.tf
@@ -1,16 +1,20 @@
+# User configuration (dynamic)
 variable "users" {
   description = "Map of users and their access configuration"
   type = map(object({
     access_type  = string
-    bucket_name = string
+    environment  = string # Added environment tag for each user
+    bucket_name  = string
   }))
   default = {
     "UserA" = {
       access_type = "read_write"
+      environment = "dev"
       bucket_name = "bucket-a"
     },
     "UserB" = {
       access_type = "read_only"
+      environment = "prod"
       bucket_name = "bucket-b"
     }
   }
