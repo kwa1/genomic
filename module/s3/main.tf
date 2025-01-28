6df0d1b3e7cbb0d7d9762a0305a47652bf567df7
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-28 01:29:11 +0000

    Update main.tf

--- module/s3/main.tf
+++ module/s3/main.tf
@@ -1,5 +1,44 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "this_logging" {
+  count = length(var.bucket_names)
+
+  bucket = var.bucket_names[count.index]
+  acl    = var.bucket_acl
+  region = var.bucket_region
+
+  lifecycle {
+    prevent_destroy = var.prevent_destroy
+  }
+
+  # Enable S3 access logging
+  logging {
+    target_bucket = var.logs_bucket_name
+    target_prefix = "logs/${var.bucket_names[count.index]}/"
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      actions = ["AES256"] 
+
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+        kms_master_key_id = "alias/aws/s3"
+      }
+    }
+  }
+
+  tags = var.tags
+}
+
+resource "aws_s3_bucket" "this_versioning" {
+  count = length(var.bucket_names)
+
+  bucket = var.bucket_names[count.index]
+  acl    = var.bucket_acl
+  region = var.bucket_region
+
+  lifecycle {
+    prevent_destroy = var.prevent_destroy
+  }
 
   dynamic "versioning" {
     for_each = var.enable_versioning ? [1] : []
@@ -8,20 +47,5 @@ resource "aws_s3_bucket" "bucket" {
     }
   }
 
-  dynamic "server_side_encryption_configuration" {
-    for_each = var.enable_encryption ? [1] : []
-    content {
-      rule {
-        apply_server_side_encryption_by_default {
-          sse_algorithm = "AES256"
-        }
-      }
-    }
-  }
-
   tags = var.tags
-}
-
-output "bucket_id" {
-  value = aws_s3_bucket.bucket.id
 }
