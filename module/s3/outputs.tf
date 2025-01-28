2225fd36ab34f9a5599141eebf0b8717e1e798ba
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-28 01:32:08 +0000

    Create outputs.tf

---
+++ module/s3/outputs.tf
@@ -0,0 +1,14 @@
+output "s3_buckets" {
+  description = "The list of S3 bucket names"
+  value       = aws_s3_bucket.this_logging[*].bucket
+}
+
+output "logging_configuration" {
+  description = "The S3 logging configuration"
+  value       = aws_s3_bucket.this_logging[*].logging
+}
+
+output "versioning_configuration" {
+  description = "The versioning configuration for the S3 buckets"
+  value       = aws_s3_bucket.this_versioning[*].versioning
+}
