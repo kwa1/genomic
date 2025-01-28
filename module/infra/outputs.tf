e99ddb0532b01502b66cc213070b5bd47665f4bd
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-28 02:08:44 +0000

    Update outputs.tf

--- module/infra/outputs.tf
+++ module/infra/outputs.tf
@@ -5,3 +5,12 @@ output "s3_bucket_names" {
 output "lambda_function_name" {
   value = module.lambda.lambda_function_name
 }
+output "lambda_function_name" {
+  description = "The name of the Lambda function"
+  value       = module.lambda.lambda_function_name
+}
+
+output "lambda_role" {
+  description = "The IAM role for the Lambda function"
+  value       = module.lambda.lambda_role
+}
