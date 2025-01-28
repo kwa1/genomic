37295c271c5756b1d227525a52c8ac152aa564d7
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-28 11:57:44 +0000

    Update outputs.tf

--- module/lambda/outputs.tf
+++ module/lambda/outputs.tf
@@ -3,7 +3,17 @@ output "lambda_function_name" {
   value       = aws_lambda_function.this.function_name
 }
 
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda function"
+  value       = aws_lambda_function.this.arn
+}
+
 output "lambda_role" {
   description = "The IAM role for the Lambda function"
   value       = aws_iam_role.lambda_role.arn
+}
+
+output "lambda_s3_notification" {
+  description = "The S3 bucket notification for Lambda"
+  value       = aws_s3_bucket_notification.lambda_trigger.id
 }
