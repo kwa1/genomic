40af8e5281559ae6039372bbe143e617fce6a4f4
Dennis Amankwa <amankwa32@hotmail.com>
2025-01-28 00:45:16 +0000

    Update main.tf

--- module/lambda/main.tf
+++ module/lambda/main.tf
@@ -1,18 +1,80 @@
-resource "aws_lambda_function" "lambda" {
-  function_name        = var.function_name
-  runtime              = var.runtime
-  handler              = var.handler
-  role                 = var.role_arn
-  filename             = var.source_code_path
-  source_code_hash     = filebase64sha256(var.source_code_path)
+resource "aws_lambda_function" "this" {
+  function_name = var.function_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "index.lambda_handler"
+  runtime       = "python3.8"
+  timeout       = 120
+
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  filename         = data.archive_file.lambda_zip.output_path
 
   environment {
     variables = var.environment_variables
   }
-
-  tags = var.tags
 }
 
-output "lambda_arn" {
-  value = aws_lambda_function.lambda.arn
+resource "aws_iam_role" "lambda_role" {
+  name = "${var.function_name}_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "${var.function_name}_policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = ["s3:GetObject", "s3:PutObject"]
+        Effect   = "Allow"
+        Resource = [
+          "arn:aws:s3:::${var.source_bucket_name}/*",
+          "arn:aws:s3:::${var.destination_bucket_name}/*"
+        ]
+      },
+      {
+        Action   = "logs:*"
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_s3_bucket_object" "this" {
+  bucket = var.source_bucket_name
+  key    = var.object_key
+  source = var.source_path
+}
+
+resource "aws_lambda_permission" "allow_s3_event" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.this.function_name
+  principal     = "s3.amazonaws.com"
+
+  source_arn = aws_s3_bucket.this.arn
+}
+
+resource "aws_s3_bucket_notification" "lambda_trigger" {
+  bucket = var.source_bucket_name
+
+  lambda_function {
+    events     = ["s3:ObjectCreated:*"]
+    lambda_function_arn = aws_lambda_function.this.arn
+    filter_prefix = var.filter_prefix
+    filter_suffix = var.filter_suffix
+  }
 }
